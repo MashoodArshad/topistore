58a02b5777517657637b0dd4095f179149f4b9e9
Mashood Arshad <mashoodarshad22@gmail.com>
2026-09-01 02:38:23 +0500

    perf(media): eliminate all black boxes, compress images, and enable 30-day browser caching

--- topistore/urls.py
+++ topistore/urls.py
@@ -3,6 +3,10 @@ from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.static import serve
+from django.views.decorators.cache import cache_control
+
+# ⚡ High-Performance Cached Media View (Caches images in user's browser for 30 days)
+cached_serve = cache_control(max_age=2592000, public=True, immutable=True)(serve)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -11,9 +15,9 @@ urlpatterns = [
     path('checkout/', include('orders.urls', namespace='orders')),
     path('', include('core.urls', namespace='core')),
     
-    # 📸 Serve user-uploaded product media and static files in live production
-    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
-    re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
+    # 📸 Fast production static and media routes with instant caching
+    re_path(r'^media/(?P<path>.*)$', cached_serve, {'document_root': settings.MEDIA_ROOT}),
+    re_path(r'^static/(?P<path>.*)$', cached_serve, {'document_root': settings.STATIC_ROOT}),
 ]
 
 if settings.DEBUG:
